61b13810b2b5bb4efb736b153833ec941ee27f73
KevinPaulWiz <kevinpaulwiz@gmail.com>
2020-09-25 18:24:44 +0300

    These are php files the were added by Kevin Paul

---
+++ OldApp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New Sample Data</title>
+</head>
+<body>
+<form>
+	<label>First Name:</label><br>
+	<input type="text" name="Name">
+	<label>Last Name:</label><br>
+	<input type="text" name="Name">
+	<label>Gender:</label><br>
+	<input type="text" name="Name">
+	<label>Name:</label><br>
+	<input type="text" name="Name">
+</form>
+</body>
+</html>
