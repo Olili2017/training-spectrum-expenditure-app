5919da91d3925bcc547ec6fa592f00b5de74abff
Fredrick Wampamba <fredrickwampamba1@gmail.com>
2021-02-24 15:48:46 +0300

    removed animation on the transition from main to expenses

--- app/src/main/java/com/ugnext/training/expenditureapp/MainActivity.java
+++ app/src/main/java/com/ugnext/training/expenditureapp/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent moveToIncomeAddition = new Intent(v.getContext(), AddIncomeActivity.class);
                 startActivity(moveToIncomeAddition);
+                overridePendingTransition(0,0);
             }
         });
     }
