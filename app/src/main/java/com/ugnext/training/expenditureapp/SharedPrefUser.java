15182e06688bb886587e1167625ccd24e61f7f65
FredrickWampamba <fredrickwampamba1@gmail.com>
2021-05-08 20:37:59 +0300

    Shared preferences also implementing Singleton Class

--- app/src/main/java/com/ugnext/training/expenditureapp/SharedPrefUser.java
+++ app/src/main/java/com/ugnext/training/expenditureapp/SharedPrefUser.java
@@ -1,39 +1,57 @@
 package com.ugnext.training.expenditureapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-public class RequestHandler {
-    private static RequestHandler instance;
-    private RequestQueue requestQueue;
+public class SharedPrefUser {
+    private static SharedPrefUser instance;
     private static Context ctx;
 
-    private RequestHandler(Context context) {
+    private static final String SHARED_PREF_NAME = "sharedprefLogin";
+    private static final String KEY_USER_EMAIL = "email";
+    private static final String KEY_USER_ID = "id";
+
+    private SharedPrefUser(Context context) {
         ctx = context;
-        requestQueue = getRequestQueue();
     }
 
-    public static synchronized RequestHandler getInstance(Context context) {
+    public static synchronized SharedPrefUser getInstance(Context context) {
         if (instance == null) {
-            instance = new RequestHandler(context);
+            instance = new SharedPrefUser(context);
         }
         return instance;
     }
 
-    public RequestQueue getRequestQueue() {
-        if (requestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
-        }
-        return requestQueue;
+    public boolean userLogin(int id, String email){
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_USER_ID, id);
+        editor.putString(KEY_USER_EMAIL, email);
+
+        editor.apply();
+
+        return true;
     }
 
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
+    public boolean isLoggedIn(){
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if (sharedPreferences.getString(KEY_USER_EMAIL, null) == null){
+            return false;
+        }
+        return  true;
+    }
+
+    public boolean logout(){
+        SharedPreferences sharedPreferences = ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        return true;
     }
 
 }
